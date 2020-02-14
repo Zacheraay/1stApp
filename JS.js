46a1b9a3172a9a0aaf2d864fd1379fc56f81c084
Acer N Cristea <ac01064@srcs.loc>
2020-02-14 14:34:07 -0800

    New JS Function

---
+++ JS.js
@@ -0,0 +1,13 @@
+function SlotMachine(){
+    var firstOption;
+
+
+
+}
+
+
+
+//3 slot machine options
+
+// randomly generate three - if you win displays message, if you lose display message - background casino based
+
