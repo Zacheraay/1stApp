e08626ea26029c214a3a09b088c37df2098e1ac1
Zachary Velyvis <zv01282@srcs.loc>
2020-02-24 10:45:05 -0800

    images + js array

--- JS.js
+++ JS.js
@@ -1,15 +1,7 @@
 function SlotMachine() {
-
-    var numbers = [ ]
-    var Slot1 = document.createElement("img");
-
-
-
-
+    var numbers = ["Images/0.png", "Images/1.png",
+                   "Images/2.png", "Images/3.png",
+                   "Images/4.png", "Images/5.png",
+                   "Images/6.png", "Images/7.png",
+                   "Images/8.png", "Images/9.png"];
 }
-
-
-
-//3 slot machine options
-
-// randomly generate three - if you win displays message, if you lose display message - background casino based
