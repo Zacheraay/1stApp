094104f3f9239b6b1d346df5c72109dd0860e591
Zachary Velyvis <zv01282@srcs.loc>
2020-02-26 13:23:06 -0800

    JS function

--- JS.js
+++ JS.js
@@ -1,12 +1,19 @@
 function slotMachine() {
+
+
+}
+
+function cycleNumber(imgNumber) {
     var numbers = ["Images/0.png", "Images/1.png",
                    "Images/2.png", "Images/3.png",
                    "Images/4.png", "Images/5.png",
                    "Images/6.png", "Images/7.png",
                    "Images/8.png", "Images/9.png"];
 
-}
+    rNum = Math.floor(Math.random() * 10);
 
-function cycleNumber() {
+    var image = document.getElementById("img"+imgNumber);
 
+    image.removeAttribute("src");
+    image.setAttribute("src", numbers[rNum]);
 }
