b011bf5d269b40f912e1b3ae7fe325876c5c5376
Zachary Velyvis <zv01282@srcs.loc>
2020-02-26 13:12:50 -0800

    img tag

--- JS.js
+++ JS.js
@@ -1,7 +1,12 @@
-function SlotMachine() {
+function slotMachine() {
     var numbers = ["Images/0.png", "Images/1.png",
                    "Images/2.png", "Images/3.png",
                    "Images/4.png", "Images/5.png",
                    "Images/6.png", "Images/7.png",
                    "Images/8.png", "Images/9.png"];
+
+}
+
+function cycleNumber() {
+
 }
